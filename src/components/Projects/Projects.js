10408d5b8e45b3668a06907ef48ad4345bb2809f
Leo Miranda <leomir2656gmail.com>
2021-08-17 12:30:43 -0400

    V2.6

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -34,19 +34,6 @@ function Projects() {
               link="https://coronavirus-chile.vercel.app/"
             />
           </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={editor}
-              isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              link="https://github.com/soumyajit4419/Editor.io"
-            />
-          </Col>
-
-         
-
          
         </Row>
       </Container>
