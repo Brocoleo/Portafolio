b1085bd5593fb492c2acc8dbd70163f044c35982
Leandro Miranda <leomir2656@gmail.com>
2022-11-30 18:32:43 -0300

    V3.2

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -11,7 +11,7 @@ function Home2() {
         <Row>
           <Col md={8} className="home-about-description">
             <h1 style={{ fontSize: "2.6em" }}>
-              UNA BREVE <span className="purple"> INTRODUCCION </span> DE QUIEN SOY
+               <span className="purple"> INTRODUCCION </span> DE QUIEN SOY
             </h1>
             <p className="home-about-body">
               Soy un programador junior especializado en el desarrollo frontend, aspiro a seguir aprendiendo nuevas 
@@ -30,9 +30,19 @@ function Home2() {
                 <b className="purple">Desarrollo Web </b>
                 en tecnologías de React y  {" "}
                 <b className="purple"> desarrollo movíl</b>
-                {" "} con React Native
+                {" "} con React Native.
               </i>
               <br />
+              <br />
+
+              
+              <i>
+                <b className="purple">Desarrollador web  </b>
+                Enexum S.A.  2022.
+                <br />
+                <b className="purple"> Soporte de T.I.  </b>
+                EPYS especialistas en procesos SPA    2022.
+              </i>
               <br />
               
             </p>
